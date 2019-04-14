f34d846c44266170273e7fc488c927e298bfb5fc
Veeravel Manivannan <veeravelpro@gmail.com>
2019-04-14 08:49:20 +0200

    [Tutorial3] , adding details view

--- python_code/PollingSite/polls/views.py
+++ python_code/PollingSite/polls/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def details(request,question_id):
+    return HttpResponse("Hello thanks for asking about a question ,this is the question id you asked for %s" % question_id)
