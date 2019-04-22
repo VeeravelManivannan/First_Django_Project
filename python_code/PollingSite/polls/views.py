084e59b80c3e670b7ac55fa938f47528b1c2960c
Veeravel Manivannan <veeravelpro@gmail.com>
2019-04-22 16:14:21 +0200

    [Tutorial 3] Making the index view access the Question Model

--- python_code/PollingSite/polls/views.py
+++ python_code/PollingSite/polls/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #Next 3 lines , original code from the tutorial
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
+
+    question_list = Question.objects.order_by('pub_date')
+    output = ', '.join(q.question_text for q in question_list)
+    print("The value for output is %s" %output)
+    return HttpResponse(output)
 
 def details(request,question_id):
-    return HttpResponse("Hello thanks for asking about a question ,this is the question id you asked for %s" % question_id)
+    return HttpResponse("(Under Implementation)Hello thanks for asking about a question ,this is the question id you asked for %s" % question_id)
+
+def results(request,question_id):
+    return HttpResponse("(Under implementation)You are now looking at the results of the question no :  %s" % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("(Under implementation ) You are looking at the no of votes for the question no :  %s" % question_id)
