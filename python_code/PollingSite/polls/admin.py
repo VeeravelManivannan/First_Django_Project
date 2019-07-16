fedfa83d970557fc53573ca86cf469c73c40f9dc
VeeravelManivannan <veeravel.se@gmail.com>
2019-07-16 16:24:03 +0200

    [Tutorial 7] Customising the look and feel of the admin site

--- python_code/PollingSite/polls/admin.py
+++ python_code/PollingSite/polls/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 from .models import Question,Choice
 
 
-# Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+#For Inline choice model(ie editing both the PARENT and CHILD model in the same admin page)
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+
+
+#Modeladmins
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ('pub_date','question_text')
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question,QuestionAdmin)    
+
+#admin.site.register(Choice)
